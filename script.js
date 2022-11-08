5943c98b1b14181fe3c7455a22b4806a5bc923aa
GeorgeDV <georgedevries909@hotmail.com>
2022-11-09 10:57:10 +1100

    pretty much done

--- script.js
+++ script.js
@@ -43,6 +43,49 @@ function cellClick(event) {
         turn = playerX;
     }
 
+    checkWinner()
 
+
+}
+
+function checkWinner() {
+    for (const winningCombo of winningCombos) {
+        const combo = winningCombo.combo;
+
+        const cellValue1 = gameBoard[combo[0] - 1];
+        const cellValue2 = gameBoard[combo[1] - 1];
+        const cellValue3 = gameBoard[combo[2] - 1];
+
+        if (
+            cellValue1 != null && cellValue2 === cellValue1 && cellValue3 === cellValue1
+        ) {
+            gameOver(cellValue1);
+        }
+    }
+
+}
+
+const winningCombos = [
+    {combo: [1, 2, 3]},
+    {combo: [4, 5, 6]},
+    {combp: [7, 8, 9]},
+
+    {combo: [1, 4, 7]},
+    {combo: [2, 5, 8]},
+    {combp: [3, 6, 9]},
+
+    {combo: [1, 5, 9]},
+    {combo: [3, 5, 7]},
+];
+
+
+function gameOver(winnnerText) {
+    let text = "Draw";
+    if (winnnerText != null) {
+        text = `Winner is ${winnnerText}`;
+    }
+
+    gameOverArea.className = "visible";
+    gameOverText.innerText = text;
 
 }
